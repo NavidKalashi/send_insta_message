5bbd82008cd9169d4139c3c0a194721cd93b45ba
Navid Kalashi <kalashinavid@gmail.com>
2023-08-11 15:46:58 +0330

    first commit

---
+++ index.py
@@ -0,0 +1,11 @@
+from instabot import Bot
+
+bot = Bot()
+
+bot.login(username='_.naaaavid._', password='n123456N')
+
+user_ids = ['eyellaaaa']
+
+text = 'I love you'
+
+bot.send_messages(text, user_ids)
